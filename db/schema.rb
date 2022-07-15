df8345aa40c19cbf8feef7ec4d11a83ed9c512f6
rutiksimform <rutik.p@simformsolutions.com>
2022-07-15 14:27:43 +0530

    resize image using variant

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_15_081110) do
+ActiveRecord::Schema.define(version: 2022_07_15_084711) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(version: 2022_07_15_081110) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.string "subtitle"
-    t.string "photo"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
