f16cec0a8b2cc81003e1355f5a8c88deeb302354
rutiksimform <rutik.p@simformsolutions.com>
2022-07-15 14:45:16 +0530

    resize image using styling

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,8 @@
 class Blog < ApplicationRecord
-  has_one_attached :photo
+  # has_one_attached :photo
+
+  # To Resize the image
+  has_one_attached :photo do |attachable|
+    attachable.variant :thumb, resize_to_limit: [350, 350]
+  end
 end
