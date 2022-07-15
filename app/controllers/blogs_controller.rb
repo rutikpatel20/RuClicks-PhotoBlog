4840d5e90b76f7dff6092f2629804ae3edced832
rutiksimform <rutik.p@simformsolutions.com>
2022-07-15 12:45:29 +0530

    add blog CRUD and styling

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,53 @@
+class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @blogs = Blog.all
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.create(blog_params)
+    if @blog.valid?
+      flash[:errors] = "Blog Created Succesfully"
+      redirect_to blogs_path
+    else
+      flash[:errors] = @blog.errors.full_messages
+      redirect_to new_blog_path
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @blog.update(blog_params)
+      flash[:errors] = "Blog Updated Successfully"
+      redirect_to blog_path(@blog)
+    else
+      flash[:errors] = @blog.errors.full_messages
+      redirect_to edit_blog_path
+    end
+  end
+
+  def destroy
+    @blog.destroy
+    redirect_to blogs_path, notice: "Blog was successfully destroyed."
+  end
+
+  private
+
+  def set_blog
+    @blog = Blog.find(params[:id])
+  end
+
+  def blog_params
+    params.require(:blog).permit(:title, :subtitle, :photo)
+  end
+end
