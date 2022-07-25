8d3c825bed8a8b994abf7b255650fc840b923a3c
rutikpatel20 <rutik.p@simformsolutions.com>
2022-07-25 19:19:08 +0530

    Static Page Controller Initial Setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'static_pages/index'
   devise_for :users, controllers: {
                        sessions: "users/sessions",
                        passwords: "users/passwords",
