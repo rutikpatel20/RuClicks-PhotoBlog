33fd09aa93a9d6289e3a3bd7800a907137f89204
rutikpatel20 <rutik.p@simformsolutions.com>
2022-07-25 11:44:48 +0530

    add custom attributes in devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_22_133319) do
+ActiveRecord::Schema.define(version: 2022_07_25_060117) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -55,6 +55,11 @@ ActiveRecord::Schema.define(version: 2022_07_22_133319) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "contact_number"
+    t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
