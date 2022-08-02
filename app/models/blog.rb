9a675d3b4fa818e4a28b166843d038464a79b282
rutikpatel20 <rutik.p@simformsolutions.com>
2022-08-02 10:20:39 +0530

    thumbnail feature added

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -7,4 +7,9 @@ class Blog < ApplicationRecord
   has_one_attached :photo do |attachable|
     attachable.variant :thumb, resize_to_limit: [350, 350]
   end
+
+  # Thumbnail Image
+  has_one_attached :thumbnail do |attachable|
+    attachable.variant :thumb, resize_to_limit: [350, 350]
+  end
 end
