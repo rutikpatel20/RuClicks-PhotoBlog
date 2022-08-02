86e00515b3324351e7b794eaafdef07c1b3b803b
rutikpatel20 <rutik.p@simformsolutions.com>
2022-08-02 21:27:11 +0530

    add username into new admin user form

--- app/admin/admin_users.rb
+++ app/admin/admin_users.rb
@@ -1,10 +1,11 @@
 ActiveAdmin.register AdminUser do
-  permit_params :email, :password, :password_confirmation
+  permit_params :email, :username, :password, :password_confirmation
 
   index do
     selectable_column
     id_column
     column :email
+    column :username
     column :current_sign_in_at
     column :sign_in_count
     column :created_at
@@ -19,6 +20,7 @@ ActiveAdmin.register AdminUser do
   form do |f|
     f.inputs do
       f.input :email
+      f.input :username
       f.input :password
       f.input :password_confirmation
     end
