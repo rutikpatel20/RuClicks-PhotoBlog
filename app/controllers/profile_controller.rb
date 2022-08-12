04f8b7ad23d7ae472705145de3970924fdcedb58
rutikpatel20 <rutik.p@simformsolutions.com>
2022-08-12 13:18:53 +0530

    follower and following system implemented

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,4 +1,20 @@
 class ProfileController < ApplicationController
+  before_action :set_user
+
   def show
+  end
+
+  def follow
+    Relationship.create_or_find_by(follower_id: current_user.id, followee_id: @user.id)
+  end
+
+  def unfollow
+    current_user.followed_users.where(follower_id: current_user.id, followee_id: @user.id).destroy_all
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
