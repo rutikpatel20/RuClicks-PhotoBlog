25db751f079c0dabef7865f2ab65627425bb5b79
rutikpatel20 <rutik.p@simformsolutions.com>
2022-08-12 13:06:38 +0530

    model setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,24 @@ class User < ApplicationRecord
     return number_of_comments
   end
 
+  # Access the Relationship Object.
+  has_many :followed_users,
+           foreign_key: :follower_id,
+           class_name: "Relationship",
+           dependent: :destroy
+
+  # Accesses the user through the relationship object.
+  has_many :followees, through: :followed_users, dependent: :destroy
+
+  # Access the Relationship Object.
+  has_many :following_users,
+           foreign_key: :followee_id,
+           class_name: "Relationship",
+           dependent: :destroy
+
+  # Accesses the user through the relationship object.
+  has_many :followers, through: :following_users, dependent: :destroy
+
   private
 
   def self.find_for_database_authentication(warden_condition)
