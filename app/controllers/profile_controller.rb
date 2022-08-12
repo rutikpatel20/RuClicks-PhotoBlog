fc894bc4b0893e0211f4092026d27e41af931380
rutikpatel20 <rutik.p@simformsolutions.com>
2022-08-12 15:16:47 +0530

    count update without refresh

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -6,10 +6,12 @@ class ProfileController < ApplicationController
 
   def follow
     Relationship.create_or_find_by(follower_id: current_user.id, followee_id: @user.id)
+    # render "profile/show"
   end
 
   def unfollow
     current_user.followed_users.where(follower_id: current_user.id, followee_id: @user.id).destroy_all
+    # render "profile/show"
   end
 
   private
