d678e82695a003483cdbc64a7b8b9e1b9ce4e981
rutikpatel20 <rutik.p@simformsolutions.com>
2022-08-17 21:51:22 +0530

    generate and setup likescontroller and routes

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,22 @@
+class LikesController < ApplicationController
+  def create
+    @like = current_user.likes.new(like_params)
+    if !@like.save
+      flash[:notice] = @like.errors.full_messages.to_sentence
+    end
+    redirect_to @like.blog
+  end
+
+  def destroy
+    @like = current_user.likes.find(params[:id])
+    blog = @like.blog
+    @like.destroy
+    redirect_to blog
+  end
+
+  private
+
+  def like_params
+    params.require(:like).permit(:blog_id)
+  end
+end
