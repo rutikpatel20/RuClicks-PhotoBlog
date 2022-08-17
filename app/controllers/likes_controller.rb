fd5fe9116aa2821d238a4cddfc5f4f8822343955
rutikpatel20 <rutik.p@simformsolutions.com>
2022-08-17 22:24:48 +0530

    add notice when someone like a blog

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,10 +1,12 @@
 class LikesController < ApplicationController
   def create
     @like = current_user.likes.new(like_params)
-    if !@like.save
-      flash[:notice] = @like.errors.full_messages.to_sentence
+    if @like.save
+      flash[:notice] = "Thank You For LIKE!!"
+      redirect_to @like.blog
+    else
+      flash[:errors] = @like.errors.full_messages
     end
-    redirect_to @like.blog
   end
 
   def destroy
